4ef4145968240a830518787b2811625b86aa8749
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-04-09 20:50:41 +0200

    rajout chemin page admin et création du modèle de données User + début de la page admin

---
+++ front/src/views/Admin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Admin">
+    <v-container>
+      <heading1> Admin </heading1>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Header from "@/components/Header.vue";
+import Heading1 from "@/components/Heading1.vue";
+
+@Component({
+  components: { Heading1, Header },
+})
+export default class Admin extends Vue {
+}
+</script>
