a2cf072bef0a646f853c89833d90b37404cf8b9f
Klab93 <37708074+Klab93@users.noreply.github.com>
2021-04-16 14:11:45 +0200

    formulaire de base

--- front/src/components/ProjectForm.vue
+++ front/src/components/ProjectForm.vue
@@ -1,14 +1,42 @@
 <template>
-  <div>
-
-  </div>
+  <v-row>
+    <v-col xs="12" sm="6">
+      <v-card class="pa-4">
+        <v-form>
+          <v-text-field
+            v-model="projectName"
+            label="Project Name"
+            required
+          ></v-text-field>
+          <v-textarea
+            v-model="projectDescription"
+            label="Project Description"
+            required
+          ></v-textarea>
+          <v-file-input
+            label="Images of the projects"
+            accept="image/*"
+          ></v-file-input>
+        </v-form>
+      </v-card>
+    </v-col>
+    <v-col xs="12" sm="6">
+      <v-card class="pa-4">
+        <Upload></Upload>
+        <v-text-field
+          label="Image title"
+        ></v-text-field>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import Upload from "@/components/Upload.vue";
 
 @Component({
-  components: {},
+  components: { Upload },
 })
 export default class ProjectForm extends Vue {
 
--- front/src/views/CreateProject.vue
+++ front/src/views/CreateProject.vue
@@ -1,14 +1,17 @@
 <template>
   <v-container>
-
+    <Heading1>Create Project</Heading1>
+    <ProjectForm></ProjectForm>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import Heading1 from "@/components/Heading1.vue";
+import ProjectForm from "@/components/ProjectForm.vue";
 
 @Component({
-  components: {},
+  components: { ProjectForm, Heading1 },
 })
 export default class CreateProject extends Vue {
 
