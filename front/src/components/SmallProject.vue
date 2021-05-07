556392014faf64b5361040c93954629a95489853
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-05-07 14:39:46 +0200

    rendu les liens dynamiques dans SmallProject : liens cassés à rétablir dans page project et student

--- front/src/components/SmallProject.vue
+++ front/src/components/SmallProject.vue
@@ -1,11 +1,21 @@
 <template>
-  <v-card href="/project" elevation="hover? 12 2" hover="hover">
+  <v-card
+    :href="`/project/${project.uuid}`"
+    elevation="hover? 12 2"
+    hover="hover"
+  >
     <v-img height="250" :src="project.thumbnailUrl" />
     <v-container class="px-1 py-4">
       <v-card-title href="/project" class="py-0 text--primary">
         {{ project.title }}
       </v-card-title>
-      <v-btn href="/profil" small text class="px-4" v-if="authorDisplay">
+      <v-btn
+        :href="`/profil/${project.author}`"
+        small
+        text
+        class="px-4"
+        v-if="authorDisplay"
+      >
         by {{ project.author }}
       </v-btn>
       <div v-if="descriptionDisplay">
