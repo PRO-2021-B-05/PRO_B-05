4d105a841c3f3c58ba165a91dca1ffcd0b65d229
PaulReeve717 <paulreeve717@gmail.com>
2021-05-21 19:33:07 +0200

    Small fixes

--- back/src/controllers/public/ImageController.ts
+++ back/src/controllers/public/ImageController.ts
@@ -25,139 +25,166 @@ import {SMS3StorageService} from '../../services/SMS3StorageService';
 
 @Controller('/projects/:projectId/images')
 export class ImageController {
-    @Inject()
-    private s3: SMS3StorageService;
-    private projectRepository = getRepository(Project);
-    private imageRepository = getRepository(Image);
+  @Inject()
+  private s3: SMS3StorageService;
+  private projectRepository = getRepository(Project);
+  private imageRepository = getRepository(Image);
 
-    @Get('/')
-    @Returns(200, Pagination).Of(Image).Groups('image.show')
-    async listAll(
-        @PathParams("projectId") projectId: string,
-        @QueryParams("offset") offset: number,
-        @QueryParams("limit") limit: number,
-    ) {
-        const project = await this.projectRepository.findOne({uuid: projectId});
+  @Get('/')
+  @(Returns(200, Pagination).Of(Image).Groups('image.show'))
+  async listAll(
+    @PathParams('projectId') projectId: string,
+    @QueryParams('offset') offset: number,
+    @QueryParams('limit') limit: number
+  ) {
+    const project = await this.projectRepository.findOne({uuid: projectId});
 
-        if (!project) {
-            throw new NotFound("Could not find requested project");
-        }
-
-        const results = deserialize(
-            await this.imageRepository.find({
-                where: {project},
-                relations: ["project"],
-                skip: offset,
-                take: limit,
-            }), {
-                type: Image,
-                groups: ['image.show']
-            });
-        const total = await this.imageRepository.count({where: {project}});
-        return new Pagination<Image>({results, total, offset, limit});
+    if (!project) {
+      throw new NotFound('Could not find requested project');
     }
 
-    @Get("/:uuid")
-    @Returns(200, Image).Groups('image.show')
-    async get(
-        @PathParams("projectId") projectId: string,
-        @PathParams("uuid") uuid: string,
-    ) {
-        const project = await this.projectRepository.findOne({uuid: projectId});
+    const results = deserialize(
+      await this.imageRepository.find({
+        where: {project},
+        relations: ['project'],
+        skip: offset,
+        take: limit,
+      }),
+      {
+        type: Image,
+        groups: ['image.show'],
+      }
+    );
+    const total = await this.imageRepository.count({where: {project}});
+    return new Pagination<Image>({results, total, offset, limit});
+  }
 
-        if (!project) {
-            throw new NotFound("Could not find requested project");
-        }
+  @Get('/:uuid')
+  @(Returns(200, Image).Groups('image.show'))
+  async get(
+    @PathParams('projectId') projectId: string,
+    @PathParams('uuid') uuid: string
+  ) {
+    const project = await this.projectRepository.findOne({uuid: projectId});
 
-        const image = await this.imageRepository.findOne({
-            where: {uuid},
-            relations: ["project"],
-        });
-
-        if (!image) {
-            throw new NotFound("Could not find requested image");
-        }
-
-        return image;
+    if (!project) {
+      throw new NotFound('Could not find requested project');
     }
 
-    @Post('/')
-    @Status(201)
-    async post(
-        @PathParams("projectId") projectId: string,
-        @MultipartFile("file") file: PlatformMulterFile,
-        @Response() response: Response,
-    ) {
-        const project = await this.projectRepository.findOne({uuid: projectId});
+    const image = await this.imageRepository.findOne({
+      where: {uuid},
+      relations: ['project'],
+    });
 
-        if (!project) {
-            throw new NotFound("Could not find requested project");
-        }
-
-        const createdImage = await this.imageRepository.create({
-            uuid: uuid.v4(),
-            title: "",
-            description: "",
-            project,
-        });
-        await this.imageRepository.save(createdImage);
-
-        this.s3.putFile("start", `${projectId}/${createdImage.uuid}`, file.buffer, {
-            'Content-Type': file.mimetype,
-        });
-
-        const thumbnail = await Jimp.read(file.buffer)
-        thumbnail.resize(250, Jimp.AUTO);
-        const buffer = await thumbnail.getBufferAsync(Jimp.MIME_PNG);
-
-        this.s3.putFile('start', `${projectId}/${createdImage.uuid}-thumbnail`, buffer, {
-            'Content-Type': Jimp.MIME_PNG,
-        });
-
-        response.location(`/api/v1/public/projects/${projectId}/images/${createdImage.uuid}`);
+    if (!image) {
+      throw new NotFound('Could not find requested image');
     }
 
-    @Put('/:uuid')
-    @Status(204)
-    async put(
-        @PathParams("projectId") projectId: string,
-        @PathParams("uuid") uuid: string,
-        @BodyParams(Image) image: Image,
-    ) {
-        const project = await this.projectRepository.findOne({uuid: projectId});
+    return image;
+  }
 
-        if (!project) {
-            throw new NotFound("Could not find requested project");
-        }
+  @Post('/')
+  @(Returns(201, Image).Groups('image.show'))
+  async post(
+    @PathParams('projectId') projectId: string,
+    @MultipartFile('file') file: PlatformMulterFile,
+    @BodyParams('title', String) title: string,
+    @BodyParams('description', String) description: string,
+    @Response() response: Response
+  ) {
+    const project = await this.projectRepository.findOne({uuid: projectId});
 
-        const existingImage = await this.imageRepository.findOne({uuid});
-        if (!existingImage) {
-            throw new NotFound("Could not find requested image");
-        }
-
-        await this.imageRepository.update({uuid}, {
-            title: image.title,
-            description: image.description,
-        });
+    if (!project) {
+      throw new NotFound('Could not find requested project');
     }
 
-    @Delete('/:uuid')
-    @Status(200)
-    async delete(
-        @PathParams("projectId") projectId: string,
-        @PathParams("uuid") uuid: string,
-    ) {
-        const project = await this.projectRepository.findOne({uuid: projectId});
+    const createdImage = await this.imageRepository.save({
+      title: title ?? '',
+      description: description ?? '',
+      project,
+    });
 
-        if (!project) {
-            throw new NotFound("Could not find requested project");
-        }
+    this.s3.putFile('start', `${projectId}/${createdImage.uuid}`, file.buffer, {
+      'Content-Type': file.mimetype,
+    });
 
-        const existingImage = await this.imageRepository.findOne({uuid});
-        if (!existingImage) {
-            throw new NotFound("Could not find requested image");
-        }
+    const thumbnail = await Jimp.read(file.buffer);
+    thumbnail.resize(250, Jimp.AUTO);
+    const buffer = await thumbnail.getBufferAsync(Jimp.MIME_PNG);
 
-        await this.imageRepository.delete({uuid});
+    this.s3.putFile(
+      'start',
+      `${projectId}/${createdImage.uuid}-thumbnail`,
+      buffer,
+      {
+        'Content-Type': Jimp.MIME_PNG,
+      }
+    );
+
+    response.location(
+      `/api/v1/public/projects/${projectId}/images/${createdImage.uuid}`
+    );
+
+    return deserialize(
+      await this.imageRepository.findOne(
+        {uuid: createdImage.uuid},
+        {relations: ['project']}
+      ),
+      {
+        type: Image,
+        groups: ['image.show'],
+      }
+    );
+  }
+
+  @Put('/:uuid')
+  @(Returns(204, Image).Groups('image.show'))
+  async put(
+    @PathParams('projectId') projectId: string,
+    @PathParams('uuid') uuid: string,
+    @BodyParams(Image) @Groups('image.update') image: Partial<Image>
+  ) {
+    const project = await this.projectRepository.findOne({uuid: projectId});
+
+    if (!project) {
+      throw new NotFound('Could not find requested project');
     }
+
+    const existingImage = await this.imageRepository.findOne({uuid});
+    if (!existingImage) {
+      throw new NotFound('Could not find requested image');
+    }
+
+    await this.imageRepository.update({uuid}, image);
+    return deserialize(
+      await this.imageRepository.findOne(
+        {uuid: existingImage.uuid},
+        {relations: ['project']}
+      ),
+      {
+        type: Image,
+        groups: ['image.show'],
+      }
+    );
+  }
+
+  @Delete('/:uuid')
+  @Status(200)
+  async delete(
+    @PathParams('projectId') projectId: string,
+    @PathParams('uuid') uuid: string
+  ) {
+    const project = await this.projectRepository.findOne({uuid: projectId});
+
+    if (!project) {
+      throw new NotFound('Could not find requested project');
+    }
+
+    const existingImage = await this.imageRepository.findOne({uuid});
+    if (!existingImage) {
+      throw new NotFound('Could not find requested image');
+    }
+
+    await this.imageRepository.delete({uuid});
+  }
 }
--- back/src/entities/Image.ts
+++ back/src/entities/Image.ts
@@ -1,48 +1,47 @@
-import {Groups, Nullable, Property, ReadOnly} from "@tsed/schema";
-import {Column, Entity, JoinTable, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
-import {Project} from "./Project";
+import {Groups, Nullable, Property, ReadOnly} from '@tsed/schema';
+import {Column, Entity, ManyToOne, PrimaryGeneratedColumn} from 'typeorm';
+
+import {Project} from './Project';
 
 @Entity()
 export class Image {
-    @PrimaryGeneratedColumn('uuid')
-    @Groups('image.show')
-    uuid: string;
+  @PrimaryGeneratedColumn('uuid')
+  @Groups('image.show')
+  uuid: string;
 
-    @Column()
-    @Nullable(String)
-    @Groups('image.show')
-    title: string;
+  @Column()
+  @Nullable(String)
+  @Groups('image.show', 'image.update')
+  title: string;
 
-    @Column()
-    @Nullable(String)
-    @Groups('image.show')
-    description: string;
+  @Column()
+  @Nullable(String)
+  @Groups('image.show', 'image.update')
+  description: string;
 
-    @Property()
-    @ReadOnly()
-    @Groups('image.show')
-    get url() {
-        if (!this.project) return "";
-        return `https://s3.studimax.ch/start/${this.project.uuid}/${this.uuid}/original`;
-    }
+  @Property()
+  @ReadOnly()
+  @Groups('image.show')
+  get url() {
+    if (!this.project) return '';
+    return `https://s3.studimax.ch/start/${this.project.uuid}/${this.uuid}/original`;
+  }
 
-    set url(value: string) {
-    }
+  set url(value: string) {}
 
-    @Property()
-    @ReadOnly()
-    @Groups('image.show')
-    get thumbnailUrl() {
-        if (!this.project) return "";
-        return `https://s3.studimax.ch/start/${this.project.uuid}/${this.uuid}/thumbnail`;
-    }
+  @Property()
+  @ReadOnly()
+  @Groups('image.show')
+  get thumbnailUrl() {
+    if (!this.project) return '';
+    return `https://s3.studimax.ch/start/${this.project.uuid}/${this.uuid}/thumbnail`;
+  }
 
-    set thumbnailUrl(value: string) {
-    }
+  set thumbnailUrl(value: string) {}
 
-    @ManyToOne(() => Project, project => project.images, {
-        onDelete: 'CASCADE',
-    })
-    @Groups('image.show', '!project.show', '!project.showAll')
-    project: Project;
+  @ManyToOne(() => Project, project => project.images, {
+    onDelete: 'CASCADE',
+  })
+  @Groups('image.show', '!project.show', '!project.showAll')
+  project: Project;
 }
