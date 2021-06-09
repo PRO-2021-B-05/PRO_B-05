f42e8ea12b9b38e81820a60c3a3610b9fbeedfd8
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-06-09 15:48:17 +0200

    Changes by kylia Commentaires smallproject

--- front/src/components/SmallProject.vue
+++ front/src/components/SmallProject.vue
@@ -1,3 +1,4 @@
+<!-- Commentaires en dessous du template -->
 <template>
   <v-card elevation="hover? 12 2" hover="hover">
     <v-card flat class="px-0 py-0" :href="`/project/${project.uuid}`">
@@ -40,21 +41,40 @@ import { IProject } from "@/model/IProject";
 @Component({
   components: {},
 })
+/**
+ * Gère l'affichage des projets sur les pages Discover et profil
+ */
 export default class SmallProject extends Vue {
   @Prop({ required: true }) private project?: IProject;
   @Prop({ default: false }) private authorDisplay!: boolean;
   @Prop({ default: false }) private descriptionDisplay!: boolean;
   @Prop({ default: false }) private modify!: boolean;
   private thumbnailURL? = "";
+
+  /**
+   * Mets à jour les images lorsque le composant est construit
+   */
   public async updateImages(): Promise<void> {
     this.thumbnailURL = this.project?.thumbnailUrl;
   }
+
+  /**
+   * initialisation du composant en chargeant toutes les images
+   */
   public async beforeUpdate(): Promise<void> {
     await this.updateImages();
   }
+
+  /**
+   * initialisation du composant en chargeant toutes les images
+   */
   public async mounted(): Promise<void> {
     await this.updateImages();
   }
+
+  /**
+   * suppression du projet
+   */
   public async deleteProject(): Promise<void> {
     if (!this.project) return;
     if (!this.project.student.uuid) return;
@@ -63,9 +83,3 @@ export default class SmallProject extends Vue {
   }
 }
 </script>
-<style>
-a {
-  text-decoration: none;
-  color: black;
-}
-</style>
