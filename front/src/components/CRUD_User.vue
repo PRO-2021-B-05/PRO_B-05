53c80a8837dd7e50d7ffd90c68668fb2270bf68c
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-06-09 15:27:17 +0200

    commentaires crud user

--- front/src/components/CRUD_User.vue
+++ front/src/components/CRUD_User.vue
@@ -44,9 +44,15 @@ import { Student } from "@/model/IStudent";
 @Component({
   components: {},
 })
+/**
+ * Gère le composant CRUD_User : un formulaire html permettant de créer et modifier un utilisateur
+ */
 export default class CRUD_User extends Vue {
   private passwordConfirm = "";
   @Prop({ default: false }) private overlay!: boolean;
+  /**
+   * indique quelle type d'action on veut effectuer
+   */
   @Prop({ default: null }) private crudAction?: "modify" | "create";
   @Prop({ default: "Create User" }) private title!: string;
   @Prop({
@@ -60,9 +66,18 @@ export default class CRUD_User extends Vue {
     },
   })
   private user!: Student;
+  /**
+   * fermer le composant overlay
+   */
   public close(): void {
     this.$emit("close");
   }
+
+  /**
+   * confirme l'action de l'overlay
+   * si l'action est acceptée, on ferme l'overlay
+   * deux actions possibles : modify ou create
+   */
   public async confirm(): Promise<void> {
     try {
       if (this.crudAction != null) {
