79e82004ced608e870c7f6ae8cb9a4dfdcd995c0
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-06-09 15:21:15 +0200

    Changes by kylia Communication

--- front/src/API/Communication.ts
+++ front/src/API/Communication.ts
@@ -6,8 +6,17 @@ import { Image } from "@/model/IImage";
 import { Pagination } from "@/model/Pagination";
 import { SERVER_ADDRESS } from "@/config";
 
+/**
+ * Classe gérant toutes les communnications entre la base de données et le client
+ */
 export class Communication {
+  /**
+   * token de session de l'utilisateur
+   */
   private token?: string;
+  /**
+   * instance axios (librairie simplifiant la communication)
+   */
   private axiosServer: AxiosInstance;
 
   /**
@@ -85,6 +94,11 @@ export class Communication {
     return response.data;
   }
 
+  /**
+   * récupère les étudiants
+   * @param offset indice du premier élément cherché
+   * @param limit nombre d'éléments à récupérer
+   */
   async getStudents(
     offset: number,
     limit: number
@@ -95,6 +109,10 @@ export class Communication {
     return response.data;
   }
 
+  /**
+   * récupère un étudiant
+   * @param uuid uuid de l'étudiant à récupérer
+   */
   async getStudent(uuid: string): Promise<Student> {
     const response = await this.axiosServer.get<Student>("/students/" + uuid);
     return {
@@ -107,11 +125,20 @@ export class Communication {
     };
   }
 
+  /**
+   * récupère le profil de l'étudiant connecté
+   */
   async getMyProfile(): Promise<Student> {
     const response = await this.axiosServer.get<Student>("/my/profile/");
     return response.data;
   }
 
+  /**
+   * modifie la description d'un étudiant
+   * @param firstname prénom de l'étudiant
+   * @param lastname nom de famille de l'étudiant
+   * @param description
+   */
   async modifyDescription(
     firstname: string,
     lastname: string,
@@ -125,10 +152,18 @@ export class Communication {
     return response.data;
   }
 
+  /**
+   * supprime un étudiant
+   * @param uuid uuid de l'étudiant à supprimer
+   */
   async deleteStudent(uuid: string): Promise<void> {
     await this.axiosServer.delete("/students/" + uuid);
   }
 
+  /**
+   * crée un nouvel étudiant
+   * @param student informations de l'étudiant à créer
+   */
   async sendCreateStudent(student: Student): Promise<void> {
     const newStudent = {
       username:
@@ -145,6 +180,10 @@ export class Communication {
     await this.axiosServer.post("/auth/registerStudent", newStudent);
   }
 
+  /**
+   * modifie un étudiant
+   * @param student étudiant modifié
+   */
   async sendModifyStudent(student: Student): Promise<void> {
     const newStudent = {
       username: student.username,
@@ -158,6 +197,11 @@ export class Communication {
     await this.axiosServer.put("/students/" + student.uuid, newStudent);
   }
 
+  /**
+   * créer un projet
+   * @param studentUuid uuid de l'étudiant qui crée un projet
+   * @param project informations du nouveau projet
+   */
   async sendCreateProject(
     studentUuid: string,
     project: { title: string; description: string }
@@ -170,6 +214,11 @@ export class Communication {
     return response.data;
   }
 
+  /**
+   * modifier un projet
+   * @param uuid du projet à modifier
+   * @param project informations du projet modifié
+   */
   async sendModifyProject(
     uuid: string,
     project: { title: string; description: string }
@@ -185,6 +234,11 @@ export class Communication {
     await this.axiosServer.delete("/my/projects/" + uuid);
   }
 
+  /**
+   * envoyer une image
+   * @param projectUuid uuid du projet auquel appartient l'image
+   * @param fileObject fichier contenant l'image
+   */
   async sendImage(
     projectUuid: string,
     fileObject: { file: File; title: string }
@@ -204,33 +258,55 @@ export class Communication {
     );
   }
 
+  /**
+   * supprimer une image
+   * @param projectUuid uuid du projet auquel appartient l'image à supprimer
+   * @param imageUuid uuid de l'image
+   */
   async deleteImage(projectUuid: string, imageUuid: string): Promise<void> {
     await this.axiosServer.delete(
       "/my/projects/" + projectUuid + "/images/" + imageUuid
     );
   }
 
+  /**
+   * Controller si les informations d'authentification sont exactes
+   * @param login un objet contenant les informations d'authentification
+   */
   async sendLogin(login: ILogin): Promise<AxiosResponse> {
     const response = await this.axiosServer.post("/auth/login", login);
     this.setToken(response.data.token);
     return response;
   }
 
+  /**
+   * Affecter un nouveau token de session au client
+   * @param token le nouveau token de session
+   */
   public setToken(token: string): void {
     this.token = token;
     localStorage.setItem("token", token);
     this.axiosServer.defaults.headers.common.Authorization = `Bearer ${token}`;
   }
 
+  /**
+   *  Supprimer le token de sessin
+   */
   public clearToken(): void {
     localStorage.removeItem("token");
     this.axiosServer.defaults.headers.common.Authorization = ``;
   }
 
+  /**
+   * Savoir si l'utilisateur est connecté
+   */
   public async isConnected(): Promise<boolean> {
     return !!this.token;
   }
 
+  /**
+   * Savoir si l'utilisateur connecté est un admin
+   */
   public async isAdmin(): Promise<boolean> {
     let admin = false;
     await this.axiosServer
