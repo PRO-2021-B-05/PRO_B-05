3561988af5fe13241dd8cc9e351bd94a5f1bb69c
PaulReeve717 <paulreeve717@gmail.com>
2021-06-10 13:05:50 +0200

    Commentaires Event

--- back/src/entities/Event.ts
+++ back/src/entities/Event.ts
@@ -3,28 +3,54 @@ import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from 'typeorm';
 
 import {Project} from './Project';
 
-// TODO: Supprimer.
+/**
+ * Classe représentant un événement (Pas utilisée pour le moment)
+ */
 @Entity()
 export class Event {
+  /**
+   * Identifiant unique de l'événement.
+   *
+   */
   @PrimaryGeneratedColumn('uuid')
   uuid: string;
 
+  /**
+   * Nom de l'événement.
+   *
+   */
   @Column()
   @Required()
   title: string;
 
+  /**
+   * Description de l'événement.
+   *
+   */
   @Column()
   @Required()
   description: string;
 
+  /**
+   * Date à laquelle l'événement commence.
+   *
+   */
   @Column()
   @Required()
   beginAt: Date;
 
+  /**
+   * Date à laquelle l'événement se termine.
+   *
+   */
   @Column()
   @Required()
   endAt: Date;
 
+  /**
+   * Projets faisant partie de l'événement.
+   *
+   */
   @OneToMany(() => Project, project => project.event)
   projects: Project[];
 }
